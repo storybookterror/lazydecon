c679c25701056e4604c0e89e7b4d87c7ae737acc
StorybookTerror <storybookterror@gmail.com>
2022-07-25 03:51:54 -0700

    Basic settings

--- LazyDecon.lua
+++ LazyDecon.lua
@@ -1,9 +1,34 @@
 local LAM2 = LibAddonMenu2
 
+LZD_INTRICATES_ALWAYS    = 1
+LZD_INTRICATES_NEVER     = 2
+LZD_INTRICATES_LEVELLING = 3
+
 local LZD = {
     name = "LazyDecon",
-    version = "0.1"
+    version = "0.1",
+
+    defaults = {
+        equip = {
+            quality = ITEM_FUNCTIONAL_QUALITY_ARTIFACT,
+            researchable = false,
+            ornates = false,
+            intricates = LZD_INTRICATES_ALWAYS,
+        },
+        glyphs = {
+            quality = ITEM_FUNCTIONAL_QUALITY_ARTIFACT,
+        },
+    },
 }
+
+-----------------------------------------------------------------------------
+-- Utilities
+-----------------------------------------------------------------------------
+
+local function LZD_IsTradeSkillFullyLevelled(tradeskill)
+    local lastRankXP, nextRankXP, curXP = GetSkillLineXPInfo(SKILL_TYPE_TRADESKILL, tradeskill + 1)
+    return nextRankXP == curXP
+end
 
 -----------------------------------------------------------------------------
 -- Settings UI Panel
@@ -21,14 +46,73 @@ local function LZD_CreateSettingsPanel()
     }
     LAM2:RegisterAddonPanel(LZD.name, panelData)
 
+    local qualityNames = {}
+    local qualityEnums = {}
+    for i = 0, ITEM_FUNCTIONAL_QUALITY_LEGENDARY do
+        local color = GetItemQualityColor(i)
+        local name = color:Colorize(GetString("SI_ITEMQUALITY", i))
+        qualityNames[i] = name
+        qualityEnums[name] = i
+    end
+
+    local intricatesOptions = {}
+    intricatesOptions[LZD_INTRICATES_ALWAYS] = GetString(SI_YES)
+    intricatesOptions[LZD_INTRICATES_NEVER] = GetString(SI_NO)
+    intricatesOptions[LZD_INTRICATES_LEVELLING] = "Only if Levelling"
+
     local options = {
         {
             type = "header",
             name = "Equipment",
         },
         {
+            type = "dropdown",
+            name = "Quality (at or below)",
+            width = "full",
+            choices = qualityNames,
+            default = qualityNames[LZD.defaults.equip.quality],
+            getFunc = function() return qualityNames[LZD.vars.equip.quality] end,
+            setFunc = function(value) LZD.vars.equip.quality = qualityEnums[value] end,
+        },
+        {
+            type = "checkbox",
+            name = "Researchable",
+            width = "full",
+            default = LZD.defaults.equip.researchable,
+            getFunc = function() return LZD.vars.equip.researchable end,
+            setFunc = function(value) LZD.vars.equip.researchable = value end,
+        },
+        {
+            type = "checkbox",
+            name = "Ornates",
+            width = "full",
+            default = LZD.defaults.equip.ornates,
+            getFunc = function() return LZD.vars.equip.ornates end,
+            setFunc = function(value) LZD.vars.equip.ornates = value end,
+        },
+        {
+            type = "dropdown",
+            name = "Intricates",
+            tooltip = "",
+            width = "full",
+            choices = intricatesOptions,
+            default = LZD.defaults.equip.intricates,
+            getFunc = function() return intricatesOptions[LZD.vars.equip.intricates] end,
+            setFunc = function(value) for i, s in ipairs(intricatesOptions) do if value == s then LZD.vars.equip.intricates = i break end end end,
+        },
+
+        {
             type = "header",
             name = "Glyphs",
+        },
+        {
+            type = "dropdown",
+            name = "Quality (at or below)",
+            width = "full",
+            choices = qualityNames,
+            default = qualityNames[LZD.defaults.glyphs.quality],
+            getFunc = function() return qualityNames[LZD.vars.glyphs.quality] end,
+            setFunc = function(value) LZD.vars.glyphs.quality = qualityEnums[value] end,
         },
     }
 
@@ -38,30 +122,36 @@ end
 -----------------------------------------------------------------------------
 -- What to Deconstruct
 -----------------------------------------------------------------------------
+local function LZD_ShouldDeconIntricate(intricate, tradeskill)
+    return not intricate or
+           LZD.vars.equip.intricates == LZD_INTRICATES_ALWAYS or
+           (LZD.vars.equip.intricates == LZD_INTRICATES_LEVELLING and
+            not LZD_IsTradeSkillFullyLevelled(tradeskill))
+end
+
 local function LZD_ShouldDeconEquipment(link)
     local isSet, setName, _, _, _, setId = GetItemLinkSetInfo(link, false)
     local researchable = CanItemLinkBeTraitResearched(link)
     local quality = GetItemLinkQuality(link)
     local crafted = IsItemLinkCrafted(link)
     local traitInfo = GetItemTraitInformationFromItemLink(link)
+    local craft = GetItemLinkCraftingSkillType(link)
 
     local ornate = traitInfo == ITEM_TRAIT_INFORMATION_ORNATE
     local intricate = traitInfo == ITEM_TRAIT_INFORMATION_INTRICATE
 
     return not isSet and
-           not researchable and
            not crafted and
-           quality ~= ITEM_FUNCTIONAL_QUALITY_LEGENDARY
+           (not researchable or LZD.vars.equip.researchable) and
+           (not ornate or LZD.vars.equip.ornate) and
+           LZD_ShouldDeconIntricate(intricate, craft) and
+           quality <= LZD.vars.equip.quality
 end
 
 local function LZD_ShouldDeconGlyphs(link)
     local quality = GetItemLinkQuality(link)
 
-    return quality == ITEM_FUNCTIONAL_QUALITY_TRASH or
-           quality == ITEM_FUNCTIONAL_QUALITY_NORMAL or
-           quality == ITEM_FUNCTIONAL_QUALITY_MAGIC or
-           quality == ITEM_FUNCTIONAL_QUALITY_ARCANE or
-           quality == ITEM_FUNCTIONAL_QUALITY_ARTIFACT
+    return quality <= LZD.vars.glyphs.quality
 end
 
 local function LZD_ShouldDecon(link)
@@ -119,6 +209,8 @@ local function LZD_RegisterHooks(inventory)
 end
 
 local function LZD_Initialize()
+    LZD.vars = ZO_SavedVars:NewAccountWide("LazyDeconVars", 1, GetWorldName(), LZD.defaults)
+
     LZD.savedSound = SOUNDS.SMITHING_ITEM_TO_EXTRACT_PLACED
     LZD_RegisterHooks(UNIVERSAL_DECONSTRUCTION.deconstructionPanel.inventory)
     LZD_RegisterHooks(SMITHING.deconstructionPanel.inventory)
