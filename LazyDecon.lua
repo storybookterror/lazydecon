d4f1b8628ea06dbb793efd8d776b0ae8a1f3d9c0
StorybookTerror <storybookterror@gmail.com>
2022-08-12 21:47:34 -0700

    Don't select items in Armory builds
    Items in armory builds are probably something you want to keep.

--- LazyDecon.lua
+++ LazyDecon.lua
@@ -362,6 +362,10 @@ local function LZD_ShouldDecon(bagId, slotIndex)
         return false
     end
 
+    if IsItemInArmory(bagId, slotIndex) then
+        return false
+    end
+
     local link = GetItemLink(bagId, slotIndex, LINK_STYLE_BRACKETS)
 
     if IsItemLinkCrafted(link) then
